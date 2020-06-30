b6affffb151a904ebc02fb3f802ef7b73eee2d14
Avishi Gupta <61486346+avishi143gupta@users.noreply.github.com>
2020-06-30 12:16:06 +0545

    Update settings.py

--- Decipher/settings.py
+++ Decipher/settings.py
@@ -24,7 +24,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o9n9@&dt-s$u(ldjcq-4gk@gtq&&z2rz-ab4g5sn!k1#%ch7gf'
+SECRET_KEY = '*****************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
