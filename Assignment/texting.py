5cb25a741f2b0430a07c3cfb7ca990f238f44630
Avishi Gupta <61486346+avishi143gupta@users.noreply.github.com>
2020-06-30 12:14:09 +0545

    Update texting.py

--- Assignment/texting.py
+++ Assignment/texting.py
@@ -3,4 +3,4 @@ import random
 New = random.randint(150000, 999999)
 Message = f'Verification Code for Assignment Project is = {New}'
 client = boto3.client('sns', 'ap-south-1')
-client.publish(PhoneNumber='+919453770920', Message=Message)
+client.publish(PhoneNumber='+91**********', Message=Message)
